d92eaded73403e57636c153754be195654d6da63
Karim Jordan <karimfromjordan@gmail.com>
2025-02-21 16:56:15 +0700

    wip

--- src/lib/guests.js
+++ src/lib/guests.js
@@ -32,7 +32,7 @@ export default [
 	{
 		name: 'Pedro Borges',
 		twitter: 'pedroborg_es',
-		image:'https://pbs.twimg.com/profile_images/1538337396479123456/buYLUd22_400x400.jpg'
+		image: 'https://pbs.twimg.com/profile_images/1538337396479123456/buYLUd22_400x400.jpg'
 	},
 	{
 		name: 'Bruno',
@@ -48,5 +48,28 @@ export default [
 		name: 'Holger Dal Mogensen',
 		github: 'sockmaster27',
 		image: 'https://avatars.githubusercontent.com/u/61235930?v=4'
+	},
+	{
+		name: 'Noam Shemesh',
+		bluesky: 'noam.sh',
+		image:
+			'https://cdn.bsky.app/img/avatar/plain/did:plc:fodwwxk4prcufv4dznipf5mi/bafkreib4zxgf6rpb56aw2exaspgwc6az4lk3km5haisf4dj6irnh6lsrce@jpeg'
+	},
+	{
+		name: 'Harsh Kothari',
+		bluesky: 'kothariharsh',
+		image:
+			'https://cdn.bsky.app/img/avatar/plain/did:plc:q7pk6kis5dht75cmeo7pucng/bafkreiazu4zcqlqfq7ruikky4ch6uortuc43zzxsxfohjvewwqwymetkvy@jpeg'
+	},
+	{
+		name: 'Grischa Erbe',
+		bluesky: 'threlte.xyz',
+		image:
+			'https://media.discordapp.net/attachments/1331956109552123965/1331956826648084511/Grischa_square_1.jpg?ex=679380f6&is=67922f76&hm=3c9b75277f984b2ccd9d06d705166c6b8826e9ba048299959bcd83b8a734b20a&=&format=webp&width=625&height=625'
+	},
+	{
+		name: 'Alexander Demin',
+		github: 'https://github.com/begoon',
+		image: 'https://avatars.githubusercontent.com/u/84461?v=4'
 	}
 ];
--- src/routes/templates/this-week-in-svelte/+page.js
+++ src/routes/templates/this-week-in-svelte/+page.js
@@ -3,12 +3,12 @@ export async function load(event) {
 	const data = event.url.searchParams.get('data');
 
 	const {
-		episode_number = 88,
+		episode_number = 94,
 		hosts = ['Paolo Ricciuti'],
-		guests = [],
+		guests = ['Alexander Demin'],
 		content = [
 			'Changelog',
-			'Live coding local storage'
+			'Go + Svelte as a hybrid SPA/MPA application'
 		]
 	} = data ? JSON.parse(data) : {};
 
